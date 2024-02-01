2233506bfc078d5806a56aad866ce9ed85f6adc2
Thiago Viana <thiago_rallfall@hotmail.com>
2024-02-01 12:04:27 -0300

    add  initilal booking component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,30 @@
+import BookingItem from '@/components/booking-item';
 import Header from '@/components/header';
+import Search from '@/components/search';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Thiago</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "eeee', ' dd 'de' MMMM", {
+            locale: ptBR
+          })}
+        </p>
+      </div>
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+          Agendamentos
+        </h2>
+        <BookingItem />
+      </div>
     </div>
   );
 }
---
+++ src/components/booking-item.tsx
@@ -0,0 +1,32 @@
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { Badge } from './ui/badge';
+import { Card, CardContent } from './ui/card';
+
+const BookingItem = () => {
+  return (
+    <Card>
+      <CardContent className="p-5 flex flex-row justify-between py-0">
+        <div className="flex flex-col gap-2 py-5">
+          <Badge className="bg-[#221c3D] text-primary hover:bg-[#221c3D] w-fit">
+            Confirmado
+          </Badge>
+          <h2 className="font-bold">Corte de Cabelo</h2>
+          <div className="flex items-center gap-2">
+            <Avatar className="w-6 h-6">
+              <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
+              <AvatarFallback>A</AvatarFallback>
+            </Avatar>
+            <h3 className="text-sm">Vintage Barber</h3>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center px-3 border-l border-solid border-secondary">
+          <p className="text-sm">Fevereiro</p>
+          <div className="text-2xl">06</div>
+          <div className="text-sm">09:45</div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default BookingItem;
