f332bbfa54684c7a97ef8b08d2bc4642bf2b0bfe
Thiago Viana <thiago_rallfall@hotmail.com>
2024-02-01 14:24:23 -0300

    add barbershop-item

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,17 @@
+import { Key } from 'react';
+
+import BarbershopItem from '@/components/barbershop-item';
 import BookingItem from '@/components/booking-item';
 import Header from '@/components/header';
 import Search from '@/components/search';
+import { db } from '@/lib/prisma';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
-export default function Home() {
+export default async function Home() {
+  // chamar prisma e pegar barbearias
+  const barbershops = await db.barbershop.findMany({});
+
   return (
     <div>
       <Header />
@@ -24,6 +31,27 @@ export default function Home() {
           Agendamentos
         </h2>
         <BookingItem />
+      </div>
+      <div className="mt-6">
+        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3 px-5">
+          Recomendados
+        </h2>
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop: { id: Key | null | undefined }) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Populares
+        </h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop: { id: Key | null | undefined }) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import './globals.css';
+import Footer from '@/components/footer';
 import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={cn(inter.className, 'dark')}>{children}</body>
+      <body className={cn(inter.className, 'dark')}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
