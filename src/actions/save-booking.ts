3dc9bdc17b483808ac48f2034fafc76a98e95214
Thiago Viana <thiago_rallfall@hotmail.com>
2024-02-29 19:39:19 -0300

    Add Service-titem and complements

---
+++ src/actions/save-booking.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+
+import { db } from '@/lib/prisma';
+
+interface SaveBookingParams {
+  barbershopId: string;
+  serviceId: string;
+  userId: string;
+  date: Date;
+}
+
+export const saveBooking = async (params: SaveBookingParams) => {
+  await db.booking.create({
+    data: {
+      serviceId: params.serviceId,
+      userId: params.userId,
+      date: params.date,
+      barbershopId: params.barbershopId
+    }
+  });
+
+  revalidatePath('/');
+  revalidatePath('/bookings');
+};
--- src/components/barbershop-info.tsx
+++ src/components/barbershop-info.tsx
@@ -1,8 +1,11 @@
 'use client';
+
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
+import SideMenu from './side-menu';
 import { Button } from './ui/button';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 
 import { Barbershop } from '@prisma/client';
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react';
@@ -29,14 +32,23 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
         >
           <ChevronLeftIcon />
         </Button>
-        <Button
-          onClick={handleBackClick}
-          size="icon"
-          variant="outline"
-          className="z-50 absolute top-4 right-4"
-        >
-          <MenuIcon />
-        </Button>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              size="icon"
+              variant="outline"
+              className="z-50 absolute top-4 right-4"
+            >
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
+
         <Image
           src={barbershop.imageUrl}
           fill
@@ -47,6 +59,7 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
           className="opacity-75"
         />
       </div>
+
       <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
         <h1 className="text-xl font-bold">{barbershop.name}</h1>
         <div className="flex items-center gap-1 mt-2">
